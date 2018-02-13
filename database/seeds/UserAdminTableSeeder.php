c057fa5cbdeeb20f54c0d2da54d2658c89978953
Unknown <cerny.vil@gmail.com>
2018-02-13 06:32:10 +0100

    + admin seeder

---
+++ database/seeds/UserAdminTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use TCG\Voyager\Models\Role;
+use TCG\Voyager\Models\User;
+
+class UserAdminTableSeeder extends Seeder
+{
+    /**
+     * Auto generated seed file.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (User::count() == 0) {
+            $role = Role::where('name', 'admin')->firstOrFail();
+
+            User::create([
+                'name'           => 'vilbur',
+                'email'          => 'cerny.vil@gmail.com',
+                'password'       => '$2y$10$IIsqO8nHHw3KG0lBK.Fj5.roC4uVxH4ShJGJluJ0nggw2hSnD896.',
+                'remember_token' => str_random(60),
+                'role_id'        => $role->id,
+            ]);
+        }
+    }
+}

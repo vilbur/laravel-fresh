382408a519f928b5cae26dfd0e53c1b80daa7243
Unknown <cerny.vil@gmail.com>
2018-02-14 13:09:50 +0100

    remove logo

--- resources/assets/js/app/components/navbar.vue
+++ resources/assets/js/app/components/navbar.vue
@@ -1,28 +1,15 @@
 <template>
-	<nav class="navbar is-transparent is-fixed-top">
-		<div class="navbar-brand">
-			<a class="navbar-item" href="https://bulma.io">
-				<img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
-			</a>
-			<div class="navbar-burger burger" @click="toggleMenu" :class="{'is-active': navIsActive}" data-target="mainNav">
-				<span></span>
-				<span></span>
-				<span></span>
-			</div>
-		</div>
 
-		<div id="mainNav" class="navbar-menu" :class="{'is-active': navIsActive}">
+	<nav class="navbar is-transparent is-fixed-top is-centered">
+		<div id="mainNav" class="navbar-menu nav-center" :class="{'is-active': navIsActive}">
 			<div class="navbar-start">
-		   		<router-link to="/"	class="navbar-item">Home</router-link>
-				<router-link to="/about"	class="navbar-item">About</router-link>
-				<div class="navbar-item has-dropdown is-hoverable">
-					<a class="navbar-link" href="/documentation/overview/start/">
-					  Docs
-					</a>
-				</div>
+				<router-link to="/"	class="navbar-item">Home</router-link>
+				<router-link to="/bio"	class="navbar-item">About</router-link>
+		   		<router-link to="/contact"	class="navbar-item">Contact</router-link>
 			</div>
 		</div>
 	</nav>
+
 </template>
 
 <script>

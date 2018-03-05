be279a33cc5271428dba396c369605482649c1d4
Unknown <cerny.vil@gmail.com>
2018-03-05 13:13:41 +0100

    + file

---
+++ resources/assets/js/vue-extensions.js
@@ -0,0 +1,8 @@
+/** Import extension for Vue
+ *
+ */
+import Buefy from "buefy";
+
+//import BulmaCarouselMin	from './../../../node_modules/bulma-carousel/dist/bulma-carousel.min.js';
+
+window.Vue.use(Buefy);

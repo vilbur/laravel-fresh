8eaf38d992cbaf16444d7d93c24981b46d32d486
Unknown <cerny.vil@gmail.com>
2018-03-05 11:17:58 +0100

    + company seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserAdminTableSeeder::class);
+        $this->call(CompanyTableSeeder::class);
+		
         $this->call(VoyagerDatabaseSeeder::class);
 
 		$this->call(VoyagerSeeder::class);

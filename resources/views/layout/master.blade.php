a142c2325c16f495f117e77c4577511f864ba358
Unknown <cerny.vil@gmail.com>
2018-04-03 16:55:11 +0200

    update

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -6,7 +6,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
 
 		<link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
-		<link href="/css/app.css" rel="stylesheet" >
+		<link href="{{ asset('/css/app.css'}}" rel="stylesheet" >
 
 		<meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
@@ -20,7 +20,7 @@
 
 		@include('partials.footer')
 
-		<script src='/js/app.js'></script>
+		<script src="{{ asset('/js/app.js'}}"></script>
     </body>
 
 
